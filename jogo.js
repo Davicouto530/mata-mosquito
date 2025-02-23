3371a9aa2b6499190ccb88a565ea69b5a3a1a431
Davi do Couto <coutodavi530@gmail.com>
2025-02-23 18:16:09 -0300

    Quase finalizando o app mata mosquito no Javascript pela Udemy.

--- jogo.js
+++ jogo.js
@@ -2,6 +2,12 @@
 let altura = 0
 let largura = 0
 
+//Variável para remoção de vidas
+let vidas = 1
+
+//Variável para o cronometro do jogo, falando quanto tempo de jogo que é
+let tempo = 10
+
 // Função para atualizar o tamanho da tela do jogo
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight // Pega a altura da tela visível
@@ -13,7 +19,44 @@ function ajustaTamanhoPalcoJogo() {
 // Chamando a função para definir os valores iniciais da tela
 ajustaTamanhoPalcoJogo()
 
+//Variável "cronometro" recebe uma função, para cada 1segundo, decrementar 1 da variável tempo
+let cronometro = setInterval(function(){
+
+    tempo -= 1//decremento da variável tempo
+
+    //Verifando, se tempo for menor que 0(zero), e não tiver perdido todos os corações, vai mostrar que você ganhou
+    if(tempo < 0){
+        clearInterval(cronometro)//Limpando a função quando acabar o tempo
+        clearInterval(criaMosquito)//Limpando a função de criar mosquito quando acabar o tempo
+        window.location.href = 'vitoria.html'//Se o usuario ganhar, vai ser redirecionado para a oágina de vitória
+    } else { 
+        document.getElementById('cronometro').innerHTML = tempo
+        //Pegando a tag HTML pelo ID "cronometro", e colocando dentro dela escrito atraves do "innerHTML" o que ta na variável "tempo"
+    }
+
+},1000)//Vai decrementar o tempo a cada 1segundo
+
 function posicaoRandomica() {
+
+    //Remover o mosquito anterior (caso exista)
+    //Verificando se já existe um mosquito 
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+        //Se existir, vai remover o anterior, e no html dentro do script tem o "setInterval()", que vai fazer aparecer outro a cada 1segundo
+
+        //Verificando, se vidas for mais que 3,muda de página
+        if(vidas > 3){
+            //Se vidas for mais que 3(acabar as vidas), vai redirecionar para outra página de GAME OVER, indicando que perdeu
+            window.location.href = 'fim_de_jogo.html'
+        } else{
+            //Se for menor, continua removendo
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"//Pegando o ID das imagens, e adicionando mais um pra cada ID, ficando "v1", "v2" e "v3", porque são 3 corações
+
+            vidas++ //Adicionando mais um na variável e mais um no ID
+        }
+        
+    }
+
     // Gera posições aleatórias dentro dos limites da tela e evita que a imagem saia da área visível
     let posicaoX = Math.floor(Math.random() * largura) - 90
     let posicaoY = Math.floor(Math.random() * altura) - 90
@@ -27,7 +70,14 @@ function posicaoRandomica() {
     // Criando um novo elemento <img> no HTML
     let mosquito = document.createElement('img') // Cria uma tag <img>
     mosquito.src = 'imagens/mosca.png' // Define a imagem do mosquito
-    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() // Aplica uma classe CSS para estilizar o mosquito
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() // Aplica as classes CSS para estilizar o mosquito
+    mosquito.id = 'mosquito'//O ID serve para verificar se existe
+
+    //Pegando o elemento "mosquito", e incluindo um evento "onclick", que quando clicar vai disparar uma função
+    mosquito.onclick = function(){
+        this.remove()
+        //O "this" ele faz referencia ao elemento HTML que executa a função, que no caso é o "mosquito", e do lado o ".remove()"
+    }
 
     // Define a posição do mosquito na tela
     mosquito.style.left = posicaoX + 'px'
